e8778e0dcd2c018cfbe955df795fe3622c1214bc
csarq <catherinesarquis@gmail.com>
2025-05-30 07:15:58 +0100

    adds JS to dispay poem and copy text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("heya");
+function displayPoem(event) {
+  event.preventDefault();
+  const poemAreaElement = document.querySelector("#poem-area");
+  poemAreaElement.classList.add("visible");
+}
+
+const formElement = document.querySelector("#poem-form");
+formElement.addEventListener("submit", displayPoem);
+
+function copyText(event) {
+  event.preventDefault();
+  const copyTextElement = document.querySelector("#poem-text").innerHTML;
+  navigator.clipboard.writeText(copyTextElement);
+  event.target.innerHTML = "Copied! 🎉";
+  event.target.classList.add("copied");
+}
+
+const copyButton = document.querySelector("#copyBtn");
+copyButton.addEventListener("click", copyText);
