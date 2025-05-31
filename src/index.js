0c16f2ea67f18de0d35450c2c53bceef039a1811
csarq <catherinesarquis@gmail.com>
2025-05-31 04:52:56 +0100

    add typewriter effect to poem

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ function displayPoem(event) {
   event.preventDefault();
   const poemAreaElement = document.querySelector("#poem-area");
   poemAreaElement.classList.add("visible");
+
+  new Typewriter("#poem-text", {
+    strings: `Joyful,<br>
+        A simple verse,<br>
+        Words dance with hopeful light,<br>
+        Bringing smiles and warmth to fill your day,<br>
+        Be well.`,
+    autoStart: true,
+    cursor: "✨",
+    delay: 100,
+  });
 }
 
 const formElement = document.querySelector("#poem-form");
